cfb4442d80e9e60fc3fed8af7d63d38401b0dd3c
Yinyin L <yinyinl@gmail.com>
2015-08-20 02:44:21 +0800

    pass progress time-stamp

--- flow/flow.go
+++ flow/flow.go
@@ -1,20 +1,23 @@
 package flow
 
 import "fmt"
+import "time"
 
 import "github.com/yinyin/infocrosswalk"
 import "github.com/yinyin/infocrosswalk/outgoing"
 import "github.com/yinyin/infocrosswalk/incoming"
 
-func Run(outAdapter outgoing.Adapter, inAdapter incoming.Adapter, bufferSize int) (errorCount int) {
+func Run(lastProgress time.Time, outAdapter outgoing.Adapter, inAdapter incoming.Adapter, bufferSize int) (resultProgress time.Time, errorCount int) {
 	if bufferSize < 1 {
 		bufferSize = 3
 	}
 
 	flowPipe := make(chan infocrosswalk.MessageContent, bufferSize)
+	resultProgress = lastProgress
 
 	go func() {
-		err := inAdapter.FetchMessage(flowPipe)
+		var err error
+		resultProgress, err = inAdapter.FetchMessage(lastProgress, flowPipe)
 		if nil != err {
 			fmt.Println("error (in: ", inAdapter, "):", err)
 		}
@@ -29,5 +32,5 @@ func Run(outAdapter outgoing.Adapter, inAdapter incoming.Adapter, bufferSize int
 			errorCount = errorCount + 1
 		}
 	}
-	return errorCount
+	return resultProgress, errorCount
 }
